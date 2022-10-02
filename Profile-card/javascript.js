053f27d5d03e9dbb13288e867222265fb38ae67f
carlduy2002 <duydhgcc200018@fpt.edu.vn>
2022-10-02 19:11:35 +0700

    new

---
+++ Profile-card/javascript.js
@@ -0,0 +1,11 @@
+function Duy(){
+    // <Image src="th.jpg"></Image>
+    // <img src="th.jpg"></img>
+    alert('Bấm vô tấm hình kiếm má mày à?')
+}
+
+function openImg(){
+    var image = document.getElementById('image');
+    var source = image.src;
+    window.open(source);
+}
